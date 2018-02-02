e11f6ee1c59d839b633cf3693fb185bc7ea07f39
iliich246 <iliich246@gmail.com>
2018-02-02 15:18:15 +0300

    Begin images crop interface

---
+++ Images/ImagesProcessorInterface.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Zalupitto
+ * Date: 02.02.2018
+ * Time: 11:55
+ */
+
+namespace Iliich246\YicmsCommon\Images;
+
+
+interface ImagesProcessorInterface
+{
+
+}
